d0df33e75267fe3ecfed07fbc8c6ef38da49f10b
DrunkerL <1417620115@qq.com>
2022-02-12 10:58:02 +0800

    todoList案例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <h1>{{msg}}</h1>
+        <!--通过父组件给子组件传递函数类型的props实现:子给父传递数据-->
+        <School :getSchoolName="getSchoolName"></School>
+        <!--通过父组件给子组件绑定一个自定义事件实现:子给父传递数据,第一种写法：使用@或者v-on-->
+        <!--<Student @drunkerl="vegeta"></Student>-->
+        <!--通过父组件给子组件绑定一个自定义事件实现:子给父传递数据,第一种写法：使用ref-->
+        <Student ref="student"></Student>
+
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import School from "@/components/School";
+    import Student from "@/components/Student";
+    export default {
+        name: "App",
+        components: {Student, School},
+        data(){
+            return {
+                msg:"你好世界"
+            }
+        },
+        methods:{
+            getSchoolName(name){
+                console.log("学校名称是：",name)
+            },
+            vegeta(name){
+                console.log("我是",name);
+            }
+        },
+        mounted() {
+            // this.$refs.student.$on('drunkerl',this.vegeta)  //绑定自定义事件
+            this.$refs.student.$once('drunkerl',this.vegeta) //绑定自定义事件（一次性）
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
